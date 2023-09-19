07fbe1d66914ab0c2b7a2b0c4a774681b8b3d51b
wenqingfan <wenqing.fan@cern.ch>
2023-09-19 13:47:20 -0700

    debugging jet cone

--- pyjetty/alice_analysis/process/user/wenqing/process_mc_ENC.py
+++ pyjetty/alice_analysis/process/user/wenqing/process_mc_ENC.py
@@ -697,6 +697,9 @@ class ProcessMC_ENC(process_mc_base.ProcessMCBase):
       cone_parts_in_truth_jet = kwargs['cone_parts_in_truth_jet']
       cone_R = kwargs['cone_R']
 
+      print('total parts inside cone',jetcone_R,'at det level',len(cone_parts_in_det_jet))
+      print('total parts inside cone',jetcone_R,'at truth level',len(cone_parts_in_truth_jet))
+
     # Todo: add additonal weight for jet pT spectrum
     # if self.rewight_pt:
     #   w_pt = 1+pow(jet_truth,0.2)
