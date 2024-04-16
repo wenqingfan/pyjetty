33dc0e75a7d553d95d2a12bc8d3fa1383eeb3092
wenqingfan <wenqing.fan@cern.ch>
2024-04-16 18:04:50 +0000

    bug fix

--- pyjetty/alice_analysis/process/user/wenqing/pythia_gen_ENC_thermal.py
+++ pyjetty/alice_analysis/process/user/wenqing/pythia_gen_ENC_thermal.py
@@ -293,7 +293,7 @@ class PythiaGenENCThermal(process_base.ProcessBase):
                 if len(matched_jet_combined)==1: # accept if there is one match only (NB: but may be used multiple times)
                     jets_combined_matched_to_pp.append(matched_jet_combined[0]) # save matched combined jet index
                     print('matched pp jet R',jetR,'pt',jet_pp.perp(),'phi',jet_pp.phi(),'eta',jet_pp.eta())
-                    print('matched combined jet index',matched_jet_combined[0],'pt',jet_combined[matched_jet_combined[0]].perp(),'phi',jet_combined[matched_jet_combined[0]].phi(),'eta',jet_combined[matched_jet_combined[0]].eta())
+                    print('matched combined jet index',matched_jet_combined[0],'pt',jets_combined[matched_jets_combined[0]].perp(),'phi',jets_combined[matched_jets_combined[0]].phi(),'eta',jets_combined[matched_jets_combined[0]].eta())
                     
                 else:
                     jets_combined_matched_to_pp.append(-1) 
@@ -312,7 +312,7 @@ class PythiaGenENCThermal(process_base.ProcessBase):
                 imatched_jet_combined = jets_combined_matched_to_pp[index_jet_pp]
                 if imatched_jet_combined > -1:
                     nmatched_pp += 1
-                    jet_combined = jets_p[imatched_jet_combined]
+                    jet_combined = jets_combined[imatched_jet_combined]
                     self.fill_matched_jets(jet_combined, jet_pp, R_label)
                     self.fill_matched_perpcone(jet_combined, jet_pp, jet_R, R_label)
 
