058d64237bbbb6b77e7c2a968e30fd4b3ec4cf3f
wenqingfan <wenqing.fan@cern.ch>
2024-06-18 18:24:55 +0000

    add rho_local_detail as new observable

--- pyjetty/alice_analysis/process/base/common_utils.py
+++ pyjetty/alice_analysis/process/base/common_utils.py
@@ -65,6 +65,8 @@ class CommonUtils(common_base.CommonBase):
       return [obs_config_dict[name]['trk_thrd'] for name in obs_subconfig_list]
     elif observable == 'rho_local':
       return [obs_config_dict[name]['trk_thrd'] for name in obs_subconfig_list]
+    elif observable == 'rho_local_detail':
+      return [obs_config_dict[name]['trk_thrd'] for name in obs_subconfig_list]
 
     # Else observable not implemented
     return [None for _ in obs_subconfig_list]
