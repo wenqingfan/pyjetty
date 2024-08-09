c39470d7363ed4033fe4d9ffd9437bde09cbb4ca
wenqingfan <wenqing.fan@cern.ch>
2024-08-09 18:36:59 +0000

    fix variable name

--- pyjetty/alice_analysis/process/user/wenqing/process_mc_ENC_2D.py
+++ pyjetty/alice_analysis/process/user/wenqing/process_mc_ENC_2D.py
@@ -290,8 +290,8 @@ class ProcessMC_ENC_2D(process_mc_base.ProcessMCBase):
     EEC_indicies2 = EEC_cb.indices2() # contains list of 2nd track in the pair
 
     for i in range(len(EEC_rs)):
-      event_index1 = _v[EEC_indicies1[i]].user_index()
-      event_index2 = _v[EEC_indicies2[i]].user_index()
+      event_index1 = c_select[EEC_indicies1[i]].user_index()
+      event_index2 = c_select[EEC_indicies2[i]].user_index()
       pairs.append(EEC_pair(event_index1, event_index2, EEC_weights[i], EEC_rs[i], jet_pt))
 
     return pairs
