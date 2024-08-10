bc530b1a036b9b9c43590b09e0c7ed8c95891559
wenqingfan <wenqing.fan@cern.ch>
2024-08-10 03:01:26 +0000

    bug fix

--- pyjetty/alice_analysis/process/user/wenqing/process_mc_ENC_2D.py
+++ pyjetty/alice_analysis/process/user/wenqing/process_mc_ENC_2D.py
@@ -253,8 +253,6 @@ class ProcessMC_ENC_2D(process_mc_base.ProcessMCBase):
       hname = 'h_jetpt_response1D_R{}_{}'.format(jetR, obs_label)
       getattr(self, hname).Fill(jet_pt_det, jet_truth.perp(), self.pt_hat)
     else:
-      # x = ([jet_pt_det, jet_truth.perp()])
-      # x_array = array.array('d', x)
       hname = 'THnF_jetpt_response1D_R{}_{}'.format(jetR, obs_label)
       getattr(self, hname).Fill(jet_pt_det, jet_truth.perp())
 
@@ -323,7 +321,7 @@ class ProcessMC_ENC_2D(process_mc_base.ProcessMCBase):
                 else:
                   hname = 'THnF_{}{:d}_response_R{}_{}'.format(observable, iRL, jetR, obs_label)
                   x = ([np.log10(d_pair.weight), d_pair.pt, np.log10(t_pair.weight), t_pair.pt])
-                  x_array = array.array('d', x)
+                  x_array = array.array('f', x)
                   getattr(self, hname).Fill(x_array)
 
               match_found = True
@@ -341,9 +339,7 @@ class ProcessMC_ENC_2D(process_mc_base.ProcessMCBase):
                 getattr(self, hname).Miss(t_pair.weight, t_pair.pt, self.pt_hat)  # NB: if RooUnfoldResponse format, applying scaling during while processing
               else:
                 hname = 'THnF_{}{:d}_response_miss_R{}_{}'.format(observable, iRL, jetR, obs_label)
-                x = ([np.log10(t_pair.weight), t_pair.pt])
-                x_array = array.array('d', x)
-                getattr(self, hname).Fill(x_array)
+                getattr(self, hname).Fill(np.log10(t_pair.weight), t_pair.pt)
       
   #---------------------------------------------------------------
   # Return EEC pairs with the input threshold cut
